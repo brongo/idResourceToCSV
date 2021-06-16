3a7c15f01d8d1d596b64760b9be287a58434789c
SamPT <d.brongo@outlook.com>
2021-06-16 11:08:05 -0700

    Create Config.h

---
+++ source/Config.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+namespace ResourceToCSV
+{
+    // Unnecessary for now. May want it later.
+    class Config
+    {
+        public:
+            std::vector<std::string> resourceFileNames;
+    };
+}
